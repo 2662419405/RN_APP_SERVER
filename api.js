e3036436609eeae63b1c9a795e7271bf4fb4f9c1
lushengyunzuo <973814225@qq.com>
2021-02-28 15:40:58 +0800

    热门电影接口完事了

--- api.js
+++ api.js
@@ -17,8 +17,9 @@ const fromDataHomeList = $ => {
       rowChild.index = index
       rowChild.link = $(this).find(".movie-name").attr("href")
       rowChild.title = $(this).find(".movie-name").attr("title")
+      rowChild.imgsrc =  $(this).find("img").attr("src")
       rowChild.time = $(this).find("em").text()
-      rowChild.time = $(this).find("span").text()
+      rowChild.timeName = $(this).find("span").text()
       obj.content.push(rowChild)
     })
     data.push(obj)
@@ -26,6 +27,50 @@ const fromDataHomeList = $ => {
   return data
 }
 
+const fromDataInnerList = $ => {
+  
+  let data = []
+  let obj = {}
+  obj.content = [] //内容列表
+  obj.sort = [] // 分类
+  obj.paging = [] //底部分页
+  obj.title = $(".container>.row").children().find('h3').text() 
+
+  obj.allMovie =  $(".hwh-page-info").find("em").eq(0).text() //所有影视数量
+  obj.active =  $(".hwh-page-info").find("em").eq(1).text() //当前页
+
+  let k = $(".pagination").eq(1).children().slice(-1).find("a").attr("href").split("/")
+  obj.allPage = k[k.length-1].slice(0,-5) //分页数量
+  
+  $(".pagination").eq(0).children().find("a").each(function(){
+    let sortarr = {}
+    sortarr.linkTitle = $(this).text()
+    sortarr.linkUrl = $(this).attr("href")
+    obj.sort.push(sortarr)
+  })
+
+  $(".pagination").eq(1).children().slice(0).each(function(){
+    let pagingarr = {}
+
+    pagingarr.linkTitle = $(this).children().text()
+    pagingarr.linkUrl = $(this).children("a").attr("href")
+
+    obj.paging.push(pagingarr)
+  })
+
+  $('.container div .row').children().each(function (index, element) {
+    let rowChild = {}
+    rowChild.index = index
+    rowChild.link = $(this).find(".movie-name").attr("href")
+    rowChild.title = $(this).find(".movie-name").attr("title")
+    rowChild.imgsrc =  $(this).find("img").attr("src")
+    rowChild.time = $(this).find(".otherinfo").text()
+    obj.content.push(rowChild)
+  })
+  data.push(obj)
+  return data
+}
+
 module.exports = {
-  fromDataHomeList,
+  fromDataHomeList,fromDataInnerList
 }
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var cheerio = require('cheerio')
 var iconv = require('iconv-lite')
 const express = require('express')
 const app = express()
-const { fromDataHomeList } = require('./api')
+const { fromDataHomeList,fromDataInnerList } = require('./api')
 
 var url = 'http://www.613767.com'
+var innerurl = 'http://www.613767.com/type/1.html'
 
 app.get('/homeList', function (req, response) {
   http
@@ -21,6 +22,28 @@ app.get('/homeList', function (req, response) {
         var chunk = iconv.decode(Buffer.concat(datas), 'utf-8')
         var $ = cheerio.load(chunk, { decodeEntities: false })
         var htmlListData = fromDataHomeList($);
+        response.json(htmlListData)
+      })
+    })
+    .on('error', function () {
+      console.log('获取数据出错！')
+    })
+})
+
+
+app.get('/innerList', function (req, response) {
+  http
+    .get(innerurl, function (res) {
+      var datas = []
+      // 获取页面数据
+      res.on('data', function (data) {
+        datas.push(data)
+      })
+      // 数据获取结束
+      res.on('end', function () {
+        var chunk = iconv.decode(Buffer.concat(datas), 'utf-8')
+        var $ = cheerio.load(chunk, { decodeEntities: false })
+        var htmlListData = fromDataInnerList($);
         response.json(htmlListData)
       })
     })
