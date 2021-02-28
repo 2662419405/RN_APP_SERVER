c83102ade1a85eacdcce1a59e5ce7453ca66009d
lushengyunzuo <973814225@qq.com>
2021-02-28 14:12:13 +0800

    首页获取接口完成

--- api.js
+++ api.js
@@ -8,7 +8,19 @@ const fromDataHomeList = $ => {
   let data = []
   $('.container .row').each(function (index, element) {
     let obj = {}
-    obj.title = $(this).children().eq(0).children('h3').text()
+    obj.content = []
+    obj.title = $(this).children().find('h3').text()
+    obj.nextLink  = $(this).children().eq(0).find("a").attr("href")
+    obj.nextLinkName = $(this).children().eq(0).find("a").text()
+    $(this).children(".movie-item-out").slice(1).each(function(index, element){
+      let rowChild = {}
+      rowChild.index = index
+      rowChild.link = $(this).find(".movie-name").attr("href")
+      rowChild.title = $(this).find(".movie-name").attr("title")
+      rowChild.time = $(this).find("em").text()
+      rowChild.time = $(this).find("span").text()
+      obj.content.push(rowChild)
+    })
     data.push(obj)
   })
   return data
